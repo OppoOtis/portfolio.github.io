df185ee30a3b6662bf0e9042a01c5161d7bd7f5d
OppoOtis <joppedek2003@gmail.com>
2023-11-01 21:56:38 +0100

    Update solarSystem.js

--- solarSystem.js
+++ solarSystem.js
@@ -7,31 +7,19 @@ const canvas = document.getElementById('solarSystem');
 const context = canvas.getContext('2d');
 
 // Set canvas size
-canvas.width = 800; // Set a fixed canvas width
-canvas.height = 600; // Set a fixed canvas height
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-// Function to resize the elements based on the canvas size
-function resizeElements() {
-    const scaleFactor = canvas.width / 800; // Adjust this value as needed
-
-    // Define the sun and planets
-    const sun = { x: canvas.width / 2, y: canvas.height / 2, radius: 50 * scaleFactor, color: 'yellow' };
-    const planets = [mercury, venus, earth]; // Add more planets as needed
-
-    planets.forEach((planet) => {
-        planet.radius = planet.initialRadius * scaleFactor;
-        planet.distance = planet.initialDistance * scaleFactor;
-    });
-
-    return { sun, planets };
-}
+// Define the sun and planets
+const sun = { x: canvas.width / 2, y: canvas.height / 2, radius: 50, color: 'yellow' };
+const planets = [mercury, venus, earth]; // Add more planets as needed
 
 // Function to draw a planet
 function drawPlanet(planet) {
     context.beginPath();
     context.arc(
-        planet.sun.x + planet.distance * Math.cos(planet.angle),
-        planet.sun.y + planet.distance * Math.sin(planet.angle),
+        sun.x + planet.distance * Math.cos(planet.angle),
+        sun.y + planet.distance * Math.sin(planet.angle),
         planet.radius,
         0,
         2 * Math.PI
@@ -45,8 +33,6 @@ function animate() {
     requestAnimationFrame(animate);
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    const { sun, planets } = resizeElements();
-
     // Draw the sun
     context.beginPath();
     context.arc(sun.x, sun.y, sun.radius, 0, 2 * Math.PI);
@@ -56,12 +42,8 @@ function animate() {
     // Update and draw the planets
     planets.forEach((planet) => {
         planet.angle += planet.speed;
-        planet.sun = sun; // Update the sun position
         drawPlanet(planet);
     });
 }
-
-// Initialize the elements with appropriate scaling
-const { sun, planets } = resizeElements();
 
 animate();
